b7ccb69356c23919beebfe0012e9af6770ce9f1a
Karang <arthur.hennequin@free.fr>
2020-06-13 13:17:41 +0200

    Add experimental free motion

--- index.js
+++ index.js
@@ -114,7 +114,77 @@ function inject (bot) {
     resetPath()
   })
 
+  class PlayerState {
+    constructor (bot, control) {
+      // Input / Outputs
+      this.pos = bot.entity.position.clone()
+      this.vel = bot.entity.velocity.clone()
+      this.onGround = bot.entity.onGround
+      this.isInWater = bot.entity.isInWater
+      this.isInLava = bot.entity.isInLava
+      this.isInWeb = bot.entity.isInWeb
+      this.isCollidedHorizontally = bot.entity.isCollidedHorizontally
+      this.isCollidedVertically = bot.entity.isCollidedVertically
+      this.jumpTicks = bot.jumpTicks
+      this.jumpQueued = bot.jumpQueued
+
+      // Input only (not modified)
+      this.yaw = bot.entity.yaw
+      this.control = control
+    }
+
+    apply (bot) {
+      bot.entity.position = this.pos
+      bot.entity.velocity = this.vel
+      bot.entity.onGround = this.onGround
+      bot.entity.isInWater = this.isInWater
+      bot.entity.isInLava = this.isInLava
+      bot.entity.isInWeb = this.isInWeb
+      bot.entity.isCollidedHorizontally = this.isCollidedHorizontally
+      bot.entity.isCollidedVertically = this.isCollidedVertically
+      bot.jumpTicks = this.jumpTicks
+      bot.jumpQueued = this.jumpQueued
+    }
+  }
+
+  function canStraightPathTo (pos) {
+    const state = new PlayerState(bot, {
+      forward: true,
+      back: false,
+      left: false,
+      right: false,
+      jump: false,
+      sprint: false,
+      sneak: false
+    })
+    const delta = pos.minus(bot.entity.position)
+    state.yaw = Math.atan2(-delta.x, -delta.z)
+    const world = { getBlock: (pos) => { return bot.blockAt(pos, false) } }
+    for (let step = 0; step < 1000; step++) {
+      bot.physics.simulatePlayer(state, world)
+      if (pos.distanceTo(state.pos) <= 2) return true
+      // TODO: check blocks to avoid
+      if (!state.onGround || state.isCollidedHorizontally) return false
+    }
+    return false
+  }
+
   function monitorMovement () {
+    // Test freemotion
+    if (stateMovements && stateMovements.allowFreeMotion && stateGoal && stateGoal.entity) {
+      const target = stateGoal.entity
+      if (canStraightPathTo(target.position)) {
+        bot.lookAt(target.position.offset(0, 1.6, 0))
+
+        if (target.position.distanceTo(bot.entity.position) > Math.sqrt(stateGoal.rangeSq)) {
+          bot.setControlState('forward', true)
+        } else {
+          bot.clearControlStates()
+        }
+        return
+      }
+    }
+
     if (stateGoal && stateGoal.hasChanged()) {
       resetPath()
     }
