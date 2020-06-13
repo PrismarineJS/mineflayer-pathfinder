17e7a6d88b289302562217b2625c924cd8c415ad
Karang <arthur.hennequin@free.fr>
2020-06-13 13:18:08 +0200

    Add multiple bots example

---
+++ examples/multiple.js
@@ -0,0 +1,58 @@
+const mineflayer = require('mineflayer')
+const { pathfinder, Movements } = require('../')
+const { GoalInvert, GoalFollow } = require('../').goals
+
+mineflayer.multiple = (bots, constructor) => {
+  const { Worker, isMainThread, workerData } = require('worker_threads')
+  if (isMainThread) {
+    const threads = []
+    for (const i in bots) {
+      threads.push(new Worker(__filename, { workerData: bots[i] }))
+    }
+  } else {
+    constructor(workerData)
+  }
+}
+
+const bots = []
+for (let i = 0; i < 40; i++) {
+  bots.push({ username: `Bot${i}` })
+}
+
+mineflayer.multiple(bots, ({ username }) => {
+  const bot = mineflayer.createBot({ username })
+
+  bot.loadPlugin(pathfinder)
+
+  bot.once('spawn', () => {
+    // Once we've spawn, it is safe to access mcData because we know the version
+    const mcData = require('minecraft-data')(bot.version)
+
+    // We create different movement generators for different type of activity
+    const defaultMove = new Movements(bot, mcData)
+    defaultMove.allowFreeMotion = true
+
+    bot.on('path_update', (results) => {
+      console.log('[' + username + '] I can get there in ' + results.path.length + ' moves. Computation took ' + results.time.toFixed(2) + ' ms.')
+    })
+
+    bot.on('goal_reached', (goal) => {
+      console.log('[' + username + '] Here I am !')
+    })
+
+    bot.on('chat', (username, message) => {
+      if (username === bot.username) return
+
+      const target = bot.players[username].entity
+      if (message === 'follow') {
+        bot.pathfinder.setMovements(defaultMove)
+        bot.pathfinder.setGoal(new GoalFollow(target, 5), true)
+      } else if (message === 'avoid') {
+        bot.pathfinder.setMovements(defaultMove)
+        bot.pathfinder.setGoal(new GoalInvert(new GoalFollow(target, 5)), true)
+      } else if (message === 'stop') {
+        bot.pathfinder.setGoal(null)
+      }
+    })
+  })
+})
