3398a4e46dc279017c2d414d1e5ec077f413f5ae
Zn10plays <72399198+Zn10plays@users.noreply.github.com>
2022-02-07 15:54:10 -0500

    Ran lint and fixed sp

--- examples/movements.js
+++ examples/movements.js
@@ -8,29 +8,26 @@ const bot = mineflayer.createBot({ username: 'Player' })
 bot.loadPlugin(pathfinder)
 
 bot.once('spawn', () => {
-
   const mcData = require('minecraft-data')(bot.version)
 
   // pathfinder comes with default moves preinitialized (a instance of the movement class)
-  // the moves come with default ligic, like how much it can fall
-  // what blocks are used to scafold, and what blocks to avoid.
+  // the moves come with default logic, like how much it can fall
+  // what blocks are used to scaffold, and what blocks to avoid.
 
   // To make custom movement edit the movements class.
   const customMoves = new Movements(bot, mcData)
-  customMoves.canDig = false; 
-  customMoves.allow1by1towers = false;
-  customMoves.scafoldingBlocks.push(mcData.itemsByName.stone.id);
-  // thing to note scafoldingBlocks are an array while other namespases are usuallys sets
-  customMoves.blocksToAvoid.add(mcData.blocksByname.carrot.id);
+  customMoves.canDig = false
+  customMoves.allow1by1towers = false
+  customMoves.scafoldingBlocks.push(mcData.itemsByName.stone.id)
+  // Thing to note scaffoldingBlocks are an array while other namespaces are usually sets
+  customMoves.blocksToAvoid.add(mcData.blocksByName.carrot.id)
 
-  // To initialize the new movements use the .setMovements method. 
+  // To initialize the new movements use the .setMovements method.
   bot.pathfinder.setMovements(defaultMove)
-  
-  bot.on('chat', function(username, message) {
-  
+
+  bot.on('chat', function (username, message) {
     if (username === bot.username) return
 
-    
     if (message === 'come') {
       const target = bot.players[username] ? bot.players[username].entity : null
       if (!target) {
@@ -39,10 +36,7 @@ bot.once('spawn', () => {
       }
       const p = target.position
 
-      
       bot.pathfinder.setGoal(new GoalNear(p.x, p.y, p.z, 1))
     }
   })
-  
-  
 })
