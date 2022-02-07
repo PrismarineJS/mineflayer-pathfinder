a75fd2635b7d11b7e5054525bbcfcdd55b84db6c
Zn10plays <72399198+Zn10plays@users.noreply.github.com>
2022-02-07 12:15:22 -0500

    Added examples

--- examples/movements.js
+++ examples/movements.js
@@ -10,21 +10,36 @@ bot.loadPlugin(pathfinder)
 bot.once('spawn', () => {
 
   const mcData = require('minecraft-data')(bot.version)
-  const defaultMove = new Movements(bot, mcData)
+
+  // pathfinder comes with default moves preinitialized (a instance of the movement class)
+  // the moves come with default ligic, like how much it can fall
+  // what blocks are used to scafold, and what blocks to avoid.
+
+  // To make custom movement edit the movements class.
+  const customMoves = new Movements(bot, mcData)
+  customMoves.canDig = false; 
+  customMoves.allow1by1towers = false;
+  customMoves.scafoldingBlocks.push(mcData.itemsByName.stone.id);
+  // thing to note scafoldingBlocks are an array while other namespases are usuallys sets
+  customMoves.blocksToAvoid.add(mcData.blocksByname.carrot.id);
+
+  // To initialize the new movements use the .setMovements method. 
+  bot.pathfinder.setMovements(defaultMove)
   
   bot.on('chat', function(username, message) {
   
     if (username === bot.username) return
 
-    const target = bot.players[username] ? bot.players[username].entity : null
+    
     if (message === 'come') {
+      const target = bot.players[username] ? bot.players[username].entity : null
       if (!target) {
         bot.chat('I don\'t see you !')
         return
       }
       const p = target.position
 
-      bot.pathfinder.setMovements(defaultMove)
+      
       bot.pathfinder.setGoal(new GoalNear(p.x, p.y, p.z, 1))
     }
   })
